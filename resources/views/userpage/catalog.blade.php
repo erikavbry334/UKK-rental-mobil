6496b2e973dad097571df3aef3ac3c60d70d4a0e
erikavbry <erikavebriyanti30304@gmail.com>
2022-11-27 01:04:16 +0700

    up1

--- resources/views/userpage/catalog.blade.php
+++ resources/views/userpage/catalog.blade.php
@@ -1,46 +1,97 @@
 @extends('userpage.layouts.main', ['title' => 'Catalog'])
 
-@include('userpage.partials.slider')
 
 @section('content')
-    <div class="amazing-deals">
+    <div class="reservation-form">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6 offset-lg-3">
-                    <div class="section-heading text-center">
-                        <h2> Armada Yang Tersedia </h2>
+                {{-- filter pencarian --}}
+                <div class="col-lg-3 col-md-12">
+                    <h3 style="background: #22b3c1;" class="text-white py-1 text-center">Filter Pencarian</h3>
+                    <div class="col-lg-12">
+                        <div id="reservation-form" role="search" class="p-2">
+                            <form action="/catalog" method="GET" class="row">
+                                <div class="col-lg-12 ">
+                                    <i class="fa fa-user"></i>
+                                    <span>Armada:</span><br>
+                                    <select class="form-control w-100" name="armada_id" placeholder="Pilih Armada">
+                                        <option value="">Semua Armada</option>
+                                        @foreach ($armadas as $armada)
+                                            <option value="{{ $armada->id }}"
+                                                {{ request()->armada_id == $armada->id ? 'selected' : '' }}>
+                                                {{ $armada->nama_armada }}</option>
+                                        @endforeach
+                                    </select>
+                                    </h4>
+                                </div>
+                                <div class="col-lg-12 ">
+                                    <i class="fa fa-globe"></i>
+                                    <span>tanggal:</span><br>
+                                    <input type="date" name="tgl_pesan" id="" value="{{ date('Y-m-d') }}"
+                                        class="form-control" value="{{ request()->tgl_pesan }}" required>
+                                    </h4>
+                                </div>
+                                <div class="col-lg-12 ">
+                                    <i class="fa fa-globe"></i>
+                                    <span>paket:</span><br>
+                                    <select class="form-control" name="paket_id" placeholder="Pilih Paket">
+                                        <option value="">Semua Paket</option>
+                                        @foreach ($pakets as $paket)
+                                            <option
+                                                value="{{ $paket->id }}"{{ request()->paket_id == $paket->id ? 'selected' : '' }}>
+                                                {{ $paket->nama_paket }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="col-lg-6">
+                                    <button type="submit" class="btn btn-primary btn-lg w-100 mt-4">Cari</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-                @foreach ($search_armadas as $armada)
-                    <div class="col-lg-6 col-sm-6">
-                        <div class="item">
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <div class="image">
-                                        <img src="{{ $armada->gambar }}" style="object-fit: cover; height: 100%">
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 align-self-center">
-                                    <div class="content">
-                                        <h4 style="font-size: 30px">{{ $armada->nama_armada }}</h4>
-                                        <div class="row">
-                                            <div class="col-6">
-                                                <h5 class="text-info">Rp. {{ number_format($armada->harga, 0, 0, '.') }}
-                                                </h5>
+                {{-- armada tersedia --}}
+                <div class="col-lg-9 col-md-12">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="section-heading text-center">
+                                <h2> Armada Yang Tersedia </h2>
+                            </div>
+                        </div>
+                        @foreach ($search_armadas as $armada)
+                            <div class="col-lg-6 col-sm-6">
+                                <div class="item">
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="image">
+                                                <img src="{{ $armada->gambar }}" style="object-fit: cover; height: 100%">
                                             </div>
                                         </div>
-                                        <i class="fas fa-fw fa-chart-area"></i>
-                                        <i class="fas fa-fw fa-chart-area"></i>
-                                        <i class="fas fa-fw fa-chart-area"></i>
-                                        <div class="main-button">
-                                            <a href="{{ url("/catalog/$armada->id?paket_id=" . $armada->paket->id . "&tgl_pesan=" . request()->tgl_pesan . "&jumlah_unit=" . request()->jumlah_unit) }}">Pesan</a>
+                                        <div class="col-lg-6 align-self-center">
+                                            <div class="content">
+                                                <h4 style="font-size: 30px">{{ $armada->nama_armada }}</h4>
+                                                <div class="row">
+                                                    <div class="col-12">
+                                                        <h5 class="text-info">Rp.
+                                                            {{ number_format($armada->harga, 0, 0, '.') }}
+                                                        </h5>
+                                                    </div>
+                                                </div>
+                                                <i class="fas fa-solid fa-users"></i>
+                                                <i class="icon-detail-grey icon-fuel" ></i>
+                                                <i class="fas fa-fw fa-chart-home"></i>
+                                                <div class="main-button">
+                                                    <a
+                                                        href="{{ url("/catalog/$armada->id?paket_id=" . $armada->paket->id . '&tgl_pesan=' . request()->tgl_pesan . '&jumlah_unit=' . request()->jumlah_unit) }}">Pesan</a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        @endforeach
                     </div>
-                @endforeach
+                </div>
             </div>
         </div>
     </div>
--- resources/views/userpage/reservation.blade.php
+++ resources/views/userpage/reservation.blade.php
@@ -6,7 +6,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h4>Book Prefered Deal Here</h4>
+                   <h3>{{$syarat->keterangan}}</h3>
                     <h2>Make Your Reservation</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt uttersi
                         labore et dolore magna aliqua is ipsum suspendisse ultrices gravida</p>
@@ -16,109 +16,6 @@
         </div>
     </div>
 
-    <div class="more-info reservation-info">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-sm-6">
-                    <div class="info-item">
-                        <i class="fa fa-phone"></i>
-                        <h4>Make a Phone Call</h4>
-                        <a href="#">+62 819 1305 4185</a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <div class="info-item">
-                        <i class="fa fa-envelope"></i>
-                        <h4>Contact Us via Email</h4>
-                        <a href="#">erikavebriyanti30304@mail.com</a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <div class="info-item">
-                        <i class="fa fa-map-marker"></i>
-                        <h4>Visit Our Offices</h4>
-                        <a href="#">24th Street North Avenue London, UK</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="reservation-form">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div id="map">
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12469.776493332698!2d-80.14036379941481!3d25.907788681148624!2m3!1f357.26927939317244!2f20.870722720054623!3f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x88d9add4b4ac788f%3A0xe77469d09480fcdb!2sSunny%20Isles%20Beach!5e1!3m2!1sen!2sth!4v1642869952544!5m2!1sen!2sth"
-                            width="100%" height="450px" frameborder="0"
-                            style="border:0; border-top-left-radius: 23px; border-top-right-radius: 23px;"
-                            allowfullscreen=""></iframe>
-                    </div>
-                </div>
-                <div class="col-lg-12">
-                    <form id="reservation-form" name="gs" method="submit" role="search" action="#">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <h4>Make Your <em>Reservation</em> Through This <em>Form</em></h4>
-                            </div>
-                            <div class="col-lg-6">
-                                <fieldset>
-                                    <label for="Name" class="form-label">Your Name</label>
-                                    <input type="text" name="Name" class="Name" placeholder="Ex. John Smithee"
-                                        autocomplete="on" required>
-                                </fieldset>
-                            </div>
-                            <div class="col-lg-6">
-                                <fieldset>
-                                    <label for="Number" class="form-label">Your Phone Number</label>
-                                    <input type="text" name="Number" class="Number" placeholder="Ex. +xxx xxx xxx"
-                                        autocomplete="on" required>
-                                </fieldset>
-                            </div>
-                            <div class="col-lg-6">
-                                <fieldset>
-                                    <label for="chooseGuests" class="form-label">Number Of Guests</label>
-                                    <select name="Guests" class="form-select" aria-label="Default select example"
-                                        id="chooseGuests" onChange="this.form.click()">
-                                        <option selected>ex. 3 or 4 or 5</option>
-                                        <option type="checkbox" name="option1" value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
-                                        <option value="4+">4+</option>
-                                    </select>
-                                </fieldset>
-                            </div>
-                            <div class="col-lg-6">
-                                <fieldset>
-                                    <label for="Number" class="form-label">Check In Date</label>
-                                    <input type="date" name="date" class="date" required>
-                                </fieldset>
-                            </div>
-                            <div class="col-lg-12">
-                                <fieldset>
-                                    <label for="chooseDestination" class="form-label">Choose Your Destination</label>
-                                    <select name="Destination" class="form-select" aria-label="Default select example"
-                                        id="chooseCategory" onChange="this.form.click()">
-                                        <option selected>ex. Switzerland, Lausanne</option>
-                                        <option value="Italy, Roma">Italy, Roma</option>
-                                        <option value="France, Paris">France, Paris</option>
-                                        <option value="Engaland, London">Engaland, London</option>
-                                        <option value="Switzerland, Lausanne">Switzerland, Lausanne</option>
-                                    </select>
-                                </fieldset>
-                            </div>
-                            <div class="col-lg-12">
-                                <fieldset>
-                                    <button class="main-button">Make Your Reservation Now</button>
-                                </fieldset>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
 
 @section('script')
