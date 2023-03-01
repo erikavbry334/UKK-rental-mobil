04d09adbf9674f5aa4ee163c2df0d60f3442a096
Wahyu Agung Laksono <82920215+wahyall@users.noreply.github.com>
2023-03-01 15:21:30 +0700

    Update LaporanController.php

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -19,7 +19,7 @@ class LaporanController extends Controller
         ]);
 
         $bulans = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
-        $pesanans = Pesanan::whereMonth('tgl_pesan', $data['bulan'])->whereYear('tgl_pesan', $data['tahun'])->get();
+        $pesanans = Pesanan::where('status', '5')whereMonth('tgl_pesan', $data['bulan'])->whereYear('tgl_pesan', $data['tahun'])->get();
         
         $data['bulan'] = $bulans[$data['bulan'] - 1];
         $pdf = Pdf::loadView('laporan.pesanan', compact('pesanans', 'data'));
